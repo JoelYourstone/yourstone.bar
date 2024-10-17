c3a19a6cee03294268e43f05b61d86d52ba83dee
Nils Fagerberg <nils.fagerberg@qlik.com>
2024-10-17 14:57:05 +0200

    Update fager.ts
    TIllagt alkoholfri gt + små uppdateringar till ingredienser på andra drinkar

--- src/drink-lists/fager.ts
+++ src/drink-lists/fager.ts
@@ -1,50 +1,37 @@
 export const drinks = [
   {
-    name: "The triumvirate of hell (bål)",
-    keywords: ["Balanserad", "Tropisk", "Kryddig"],
-    ingredients: [
-      "Rum-bar silver (Jamaica)",
-      "Lime",
-      "Passionsfrukt",
-      "Muskot",
-      "Bitters",
-    ],
+      name: "The triumvirate of hell (bål)",
+      keywords: ["Balanserad", "Tropisk", "Kryddig"],
+      ingredients: ["Rum-bar Silver (Jamaica)", "Lime", "Passionsfrukt", "Muskot", "Bitters"]
   },
   {
-    name: "Spooky Island volcano",
-    keywords: ["Söt", "Tropisk", "Lätt"],
-    ingredients: [
-      "Mount Gay Eclipse (Barbados)",
-      "Lime",
-      "Ananas",
-      "Kokos",
-      "Guava soda",
-    ],
+      name: "Spooky Island volcano",
+      keywords: ["Söt", "Tropisk", "Lätt"],
+      ingredients: ["Mount Gay Eclipse (Barbados)", "Lime", "Ananas", "Kokos", "Guava soda"]
   },
   {
-    name: "Melloween",
-    keywords: ["Söt", "Boozy", "Mellow"],
-    ingredients: ["MFC", "Sockerlag", "Bitters", "Eld"],
+      name: "Melloween",
+      keywords: ["Söt", "Boozy", "Mellow"],
+      ingredients: ["MFC", "Sockerlag", "Bitters", "Eld"]
   },
   {
-    name: "Scooby Snaquiri (frozen)",
-    keywords: ["Söt", "Syrlig", "Stark", "Vuxenslushie"],
-    ingredients: ["Havana Club Añejo 3 años", "Lime", "Sockerlag", "Is"],
+      name: "Scooby Snaquiri (frozen)",
+      keywords: ["Söt", "Syrlig", "Stark", "Vuxenslushie"],
+      ingredients: ["Havana Club Añejo 3 años", "Lime", "Sockerlag", "Is"]
   },
   {
-    name: "Luna Ghost",
-    keywords: ["Dessert-ig", "Söt"],
-    ingredients: [
-      "Tempus Fugit Crème de Cacao & Crème de Banane",
-      "El Dorado 8 year (Guyana)",
-      "Soja- eller havregrädde",
-      "Bitter Truth Spiced Chocolate Bitters",
-      "Muskot",
-    ],
+      name: "Luna Ghost",
+      keywords: ["Dessert-ig", "Söt"],
+      ingredients: ["Tempus Fugit Crème de Cacao & Crème de Banane", "El Dorado 8 year (Guyana)", "Soja- eller havregrädde", "Bitter Truth Spiced Chocolate Bitters", "Muskot"]
   },
   {
-    name: "Champagne",
-    keywords: ["Champagne"],
-    ingredients: ["Champagne"],
+      name: "Alkoholfri Gin&Tonic (~0.4% ABV)",
+      keywords: ["Bitter", "Söt", "Frisk"],
+      ingredients: ["Hemgjord Gin Tincture på Stjernsunds Navy Gin", "Tonic Water", "Limeklyfta"]
+  },
+  {
+      name: "Champagne",
+      keywords: ["Champagne"],
+      ingredients: ["Champagne"]
   },
 ];
